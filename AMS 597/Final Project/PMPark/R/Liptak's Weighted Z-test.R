8db2836be0e86d48d089ba275eb3eee0962fd56b
Haein Park <steve121212@naver.com>
2021-05-01 11:30:51 -0400

    Eliminated Unnecessary Arguments and Added Variance Testing.

--- AMS 597/Final Project/PMPark/R/Liptak's Weighted Z-test.R
+++ AMS 597/Final Project/PMPark/R/Liptak's Weighted Z-test.R
@@ -1,5 +1,5 @@
 # Liptak's Weighted Z-test
-weighted.z.test <- function(x, y, alternative=c("two.sided", "greater", "equal"), mu=0, var.equal=F, conf.level=0.95){
+weighted.z.test <- function(x, y, alternative="two.sided", var.equal=F){
   if(is.null(x) | is.null(y)){
     stop("Both of the input vectors should not be NULL")
   }
@@ -16,34 +16,38 @@ weighted.z.test <- function(x, y, alternative=c("two.sided", "greater", "equal")
     stop("Test cannot be conducted due to the small size of sample")
   }
   
+  if(var.test(x,y)$p.value <= 0.05){
+    equal = F
+  }
+  else{
+    equal = T
+  }
+  
   n1 <- length(which(!is.na(x)&!is.na(y))) # The number of complete matched pairs
   n2 <- length(which(!is.na(x)&is.na(y))) # The number of samples that are not NA in x but NA in y
   n3 <- length(which(is.na(x)&!is.na(y))) # The number of samples that are NA in x but not in y
   w1 <- sqrt(n1) # weight of the complete matched pairs
   w2 <- sqrt(n2+n3) # weight of the partially matched pairs
-  p <- NULL
-  Z1 <- NULL
-  Z2 <- NULL
-  message <- NULL
+  
   if (alternative == "greater"){
-    p1 <- t.test(x[which(!is.na(x)&!is.na(y))], y[which(!is.na(x)&!is.na(y))], alternative = "greater", paired = T, var.equal=var.equal, conf.level = conf.level)$p.value
-    p2 <- t.test(x[which(!is.na(x)&is.na(y))], y[which(is.na(x)&!is.na(y))], alternative = "greater", paired = F, var.equal=var.equal, conf.level = conf.level)$p.value
+    p1 <- t.test(x[which(!is.na(x)&!is.na(y))], y[which(!is.na(x)&!is.na(y))], alternative = "greater", paired = T, var.equal=equal)$p.value
+    p2 <- t.test(x[which(!is.na(x)&is.na(y))], y[which(is.na(x)&!is.na(y))], alternative = "greater", paired = F, var.equal=equal)$p.value
     Z1 <- qnorm(1-p1)
     Z2 <- qnorm(1-p2)
     p <- 1 - pnorm((w1*Z1 + w2*Z2)/sqrt(w1^2 + w2^2))
-    message <- "alternative hypothesis: true difference in means is greater than"
+    message <- "alternative hypothesis: true difference in means is greater than 0"
   }
   else if (alternative == "less"){
-    p1 <- t.test(x[which(!is.na(x)&!is.na(y))], y[which(!is.na(x)&!is.na(y))], alternative = "less", paired = T, var.equal=var.equal, conf.level = conf.level)$p.value
-    p2 <- t.test(x[which(!is.na(x)&is.na(y))], y[which(is.na(x)&!is.na(y))], alternative = "less", paired = F, var.equal=var.equal, conf.level = conf.level)$p.value
+    p1 <- t.test(x[which(!is.na(x)&!is.na(y))], y[which(!is.na(x)&!is.na(y))], alternative = "less", paired = T, var.equal=equal)$p.value
+    p2 <- t.test(x[which(!is.na(x)&is.na(y))], y[which(is.na(x)&!is.na(y))], alternative = "less", paired = F, var.equal=equal)$p.value
     Z1 <- qnorm(1-p1)
     Z2 <- qnorm(1-p2)
     p <- 1 - pnorm((w1*Z1 + w2*Z2)/sqrt(w1^2 + w2^2))
-    message <- "alternative hypothesis: true difference in means is less than"
+    message <- "alternative hypothesis: true difference in means is less than 0"
   }
   else{
-    p1 <- t.test(x[which(!is.na(x)&!is.na(y))], y[which(!is.na(x)&!is.na(y))], alternative = "greater", paired = T, var.equal=var.equal, conf.level = conf.level)$p.value
-    p2 <- t.test(x[which(!is.na(x)&is.na(y))], y[which(is.na(x)&!is.na(y))], alternative = "greater", paired = F, var.equal=var.equal, conf.level = conf.level)$p.value
+    p1 <- t.test(x[which(!is.na(x)&!is.na(y))], y[which(!is.na(x)&!is.na(y))], alternative = "greater", paired = T, var.equal=equal)$p.value
+    p2 <- t.test(x[which(!is.na(x)&is.na(y))], y[which(is.na(x)&!is.na(y))], alternative = "greater", paired = F, var.equal=equal)$p.value
     Z1 <- qnorm(1-p1)
     Z2 <- qnorm(1-p2)
     p <- 1 - pnorm((w1*Z1 + w2*Z2)/sqrt(w1^2 + w2^2))
@@ -53,10 +57,8 @@ weighted.z.test <- function(x, y, alternative=c("two.sided", "greater", "equal")
     else{
       p <- 2*(1-p)
     }
-    message <- "alternative hypothesis: true difference in means is not equal to"
+    message <- "alternative hypothesis: true difference in means is not equal to 0"
   }
   
-  cat("       ", "Liptak's Weighted Z-test\n\n", "p-value =", p, "\n", "alternative hypothesis:", message, mu, "\n", "number of matched:", n1, "\n", "number of partially matched pairs:", n2+n3, "\n", "weight of matched pairs:", w1, "\n", "weight of partially matched pairs", w2, "\n", "Z score of matched pairs:", Z1, "\n", "Z score of partially matched pairs:", Z2, "\n")
-  result <- list(p.value=p, weight1=w1, weight2=w2, Z.score1=Z1, Z.score2=Z2)
-  return(result)
+  cat("       ", "Liptak's Weighted Z-test\n\n", "p-value =", p, "\n", "alternative hypothesis:", message, "\n", "number of matched:", n1, "\n", "number of partially matched pairs:", n2+n3, "\n", "weight of matched pairs:", w1, "\n", "weight of partially matched pairs", w2, "\n", "Z score of matched pairs:", Z1, "\n", "Z score of partially matched pairs:", Z2, "\n")
 }
